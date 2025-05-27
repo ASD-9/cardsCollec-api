1ee8ebb081610e84c649b826e6f4dc4a7a99417f
ASD-9 <ryan.chavatte@gmail.com>
2025-05-27 20:02:36 +0200

    add cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 const authenticate = require("./middlewares/authenticate");
 const authorize = require("./middlewares/authorize");
@@ -8,11 +9,13 @@ const app = express();
 // Disabling the “X-Powered-By” header for security reasons
 app.disable("x-powered-by");
 
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Static files
-app.use('/images', express.static('public/images'));
+app.use("/images", express.static("public/images"));
 
 // Routes
 app.use("/auth", routes.authRouter);
